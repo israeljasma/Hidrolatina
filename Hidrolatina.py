a87956c108e2aedcdd9feffe90524670a6c97fba
Israel Jasma <by.darklancer@gmail.com>
2021-06-28 20:07:52 -0400

    Feat: Cargar OD API

--- Hidrolatina.py
+++ Hidrolatina.py
@@ -90,6 +90,42 @@ def importMDETER():
 def clearCacheMDETR():
     torch.cuda.empty_cache()
 
+def loadODAPI(PATH_TO_CFG, PATH_TO_CKPT):
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    # Suppress TensorFlow logging
+    import tensorflow as tf
+    from object_detection.utils import label_map_util
+    from object_detection.utils import config_util
+    from object_detection.utils import visualization_utils as viz_utils
+    from object_detection.builders import model_builder
+
+    tf.get_logger().setLevel('ERROR')           # Suppress TensorFlow logging (2)
+
+    # # Enable GPU dynamic memory allocation
+    # gpus = tf.config.experimental.list_physical_devices('GPU')
+    # for gpu in gpus:
+    #     tf.config.experimental.set_memory_growth(gpu, True)
+
+    #Set CPU
+    tf.config.set_visible_devices([], 'GPU')
+
+    # Load pipeline config and build a detection model
+    configs = config_util.get_configs_from_pipeline_file(PATH_TO_CFG)
+    model_config = configs['model']
+    detection_model = model_builder.build(model_config=model_config, is_training=False)
+
+    # Restore checkpoint
+    ckpt = tf.train.Checkpoint(model=detection_model)
+    ckpt.restore(os.path.join(PATH_TO_CKPT, 'ckpt-0')).expect_partial()
+
+    @tf.function
+    def detect_fn(image):
+        """Detect objects in image."""
+
+        image, shapes = detection_model.preprocess(image)
+        prediction_dict = detection_model.predict(image, shapes)
+        detections = detection_model.postprocess(prediction_dict, shapes)
+
+        return detections, prediction_dict, tf.reshape(shapes, [-1])
 # if platform.system() == "Darwin":
 #     print("MacOS")
 # elif platform.system() == "Linux":
@@ -310,8 +346,9 @@ downloadModels = Button(root, text="Modelos", command=openDownloadModelsTk).pack
 
 messagebuton = Button(root, text="Popup", command=popup).pack()
 
-importLibraryButton = Button(root, text='Cargar liberias', command=importMDETER).pack()
-clearMDETRyButton = Button(root, text='Limipiar MDETR', command=clearCacheMDETR).pack()
+importLibraryButton = Button(root, text='Cargar librerias', command=importMDETER).pack()
+clearMDETRyButton = Button(root, text='Limpiar MDETR', command=clearCacheMDETR).pack()
+# loadODAPIButton = Button(root, text='Cargar OD API', command=loadODAPI(PATH_TO_CFG, PATH_TO_CKPT)).pack()
 
 exitButton = Button(root, text="Salir", command=root.quit)
 exitButton.pack()
