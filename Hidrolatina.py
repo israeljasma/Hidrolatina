e5b1ce3e7bfd449373d99fba60856aa0db85d975
Israel Jasma <by.darklancer@gmail.com>
2021-07-01 23:09:12 -0400

    Feat + Fix: MDETR Tool and little fixes

--- Hidrolatina.py
+++ Hidrolatina.py
@@ -1,3 +1,4 @@
+from glob import glob
 from sys import path
 from tkinter import *
 from tkinter import ttk
@@ -89,6 +90,147 @@ def importMDETER():
     model, postprocessor = torch.hub.load('ashkamath/mdetr:main', 'mdetr_efficientnetB5', pretrained=True, return_postprocessor=True)
     model = model.cuda()
     model.eval();
+
+    global transform, box_cxcywh_to_xyxy, rescale_bboxes, COLORS, apply_mask, plot_results, id2answerbytype, plot_inference, plot_inference_qa
+    # standard PyTorch mean-std input image normalization
+    transform = T.Compose([
+        T.Resize(800),
+        T.ToTensor(),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    # for output bounding box post-processing
+    def box_cxcywh_to_xyxy(x):
+        x_c, y_c, w, h = x.unbind(1)
+        b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+            (x_c + 0.5 * w), (y_c + 0.5 * h)]
+        return torch.stack(b, dim=1)
+
+    def rescale_bboxes(out_bbox, size):
+        img_w, img_h = size
+        b = box_cxcywh_to_xyxy(out_bbox)
+        b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+        return b
+
+    # colors for visualization
+    COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+            [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+
+    def apply_mask(image, mask, color, alpha=0.5):
+        """Apply the given mask to the image.
+        """
+        for c in range(3):
+            image[:, :, c] = np.where(mask == 1,
+                                    image[:, :, c] *
+                                    (1 - alpha) + alpha * color[c] * 255,
+                                    image[:, :, c])
+        return image
+
+    def plot_results(pil_img, scores, boxes, labels, masks=None):
+        plt.figure(figsize=(16,10))
+        np_image = np.array(pil_img)
+        ax = plt.gca()
+        colors = COLORS * 100
+        if masks is None:
+            masks = [None for _ in range(len(scores))]
+        assert len(scores) == len(boxes) == len(labels) == len(masks)
+        for s, (xmin, ymin, xmax, ymax), l, mask, c in zip(scores, boxes.tolist(), labels, masks, colors):
+            ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                    fill=False, color=c, linewidth=3))
+            text = f'{l}: {s:0.2f}'
+            ax.text(xmin, ymin, text, fontsize=15, bbox=dict(facecolor='white', alpha=0.8))
+
+            if mask is None:
+                continue
+            np_image = apply_mask(np_image, mask, c)
+
+            padded_mask = np.zeros((mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
+            padded_mask[1:-1, 1:-1] = mask
+            contours = find_contours(padded_mask, 0.5)
+            for verts in contours:
+                # Subtract the padding and flip (y, x) to (x, y)
+                verts = np.fliplr(verts) - 1
+                p = Polygon(verts, facecolor="none", edgecolor=c)
+                ax.add_patch(p)
+
+
+        plt.imshow(np_image)
+        plt.axis('off')
+        plt.show()
+
+    import json
+    answer2id_by_type = json.load(requests.get("https://nyu.box.com/shared/static/j4rnpo8ixn6v0iznno2pim6ffj3jyaj8.json", stream=True).raw)
+    id2answerbytype = {}                                                       
+    for ans_type in answer2id_by_type.keys():                        
+        curr_reversed_dict = {v: k for k, v in answer2id_by_type[ans_type].items()}
+        id2answerbytype[ans_type] = curr_reversed_dict 
+
+
+    def plot_inference(im, caption):
+    # mean-std normalize the input image (batch-size: 1)
+        img = transform(im).unsqueeze(0).cuda()
+
+        # propagate through the model
+        memory_cache = model(img, [caption], encode_and_save=True)
+        outputs = model(img, [caption], encode_and_save=False, memory_cache=memory_cache)
+
+        # keep only predictions with 0.7+ confidence
+        probas = 1 - outputs['pred_logits'].softmax(-1)[0, :, -1].cpu()
+        keep = (probas > 0.7).cpu()
+
+        # convert boxes from [0; 1] to image scales
+        bboxes_scaled = rescale_bboxes(outputs['pred_boxes'].cpu()[0, keep], im.size)
+
+        # Extract the text spans predicted by each box
+        positive_tokens = (outputs["pred_logits"].cpu()[0, keep].softmax(-1) > 0.1).nonzero().tolist()
+        predicted_spans = defaultdict(str)
+        for tok in positive_tokens:
+            item, pos = tok
+            if pos < 255:
+                span = memory_cache["tokenized"].token_to_chars(0, pos)
+                predicted_spans [item] += " " + caption[span.start:span.end]
+        labels = [predicted_spans [k] for k in sorted(list(predicted_spans .keys()))]
+        global bboxes
+        bboxes=bboxes_scaled.numpy()
+        print('boxes: ', bboxes )
+        # plot_results(im, probas[keep], bboxes_scaled, labels)
+
+    def plot_inference_qa(im, caption):
+        # mean-std normalize the input image (batch-size: 1)
+        img = transform(im).unsqueeze(0).cuda()
+
+        # propagate through the model
+        memory_cache = model_qa(img, [caption], encode_and_save=True)
+        outputs = model_qa(img, [caption], encode_and_save=False, memory_cache=memory_cache)
+
+        # keep only predictions with 0.7+ confidence
+        probas = 1 - outputs['pred_logits'].softmax(-1)[0, :, -1].cpu()
+        keep = (probas > 0.7).cpu()
+
+        # convert boxes from [0; 1] to image scales
+        bboxes_scaled = rescale_bboxes(outputs['pred_boxes'].cpu()[0, keep], im.size)
+
+        # Extract the text spans predicted by each box
+        positive_tokens = (outputs["pred_logits"].cpu()[0, keep].softmax(-1) > 0.1).nonzero().tolist()
+        predicted_spans = defaultdict(str)
+        for tok in positive_tokens:
+            item, pos = tok
+            if pos < 255:
+                span = memory_cache["tokenized"].token_to_chars(0, pos)
+                predicted_spans [item] += " " + caption[span.start:span.end]
+
+        labels = [predicted_spans [k] for k in sorted(list(predicted_spans .keys()))]
+        # plot_results(im, probas[keep], bboxes_scaled, labels)
+
+        # Classify the question type
+        type_conf, type_pred = outputs["pred_answer_type"].softmax(-1).max(-1)
+        ans_type = type_pred.item()
+        types = ["obj", "attr", "rel", "global", "cat"]
+
+        ans_conf, ans = outputs[f"pred_answer_{types[ans_type]}"][0].softmax(-1).max(-1)
+        global answer
+        answer = id2answerbytype[f"answer_{types[ans_type]}"][ans.item()]
+        print(f"Predicted answer: {answer}\t confidence={round(100 * type_conf.item() * ans_conf.item(), 2)}")
 
 def clearCacheMDETR():
     torch.cuda.empty_cache()
@@ -202,7 +344,7 @@ def pytorchCamera():
         image_path=[image_np]
 
         
-        threshold = 0.5
+        threshold = 0.8
         iou_threshold = 0.5
 
         # # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
@@ -285,6 +427,7 @@ def pytorchCamera():
             ymin = int((detected_boxes[1]))
             xmax = int((detected_boxes[2]))
             ymax = int((detected_boxes[3]))
+            global cropped_img
             cropped_img = image_np[ymin:ymax,xmin:xmax]
             global imagencamera
             if not cropped_img.size == 0:
@@ -304,16 +447,19 @@ def pytorchCamera():
             # imagencamera = Image.fromarray(cropped_img, 'RGB')
             
 
-def prefunctionclip():
-    class_names=['mask and headphones', 'mask and goggles ','mask, headphones and goggles','mask', 'goggles', 'no_mask']
-    class_names
-    im = Image.fromarray(cv2.cvtColor(cropped_img, cv2.COLOR_BGR2RGB))
+def MDETR():
+    import cv2
+    # im = Image.fromarray(cv2.cvtColor(cropped_img, cv2.COLOR_BGR2RGB))
+    im = Image.fromarray(imagencamera)
     plot_inference(im, "a hand")
     im_hand=im.crop((bboxes[len(bboxes)-1]))
     plot_inference(im, "a tiny head")
     im_head=im.crop((bboxes[len(bboxes)-1]))
-    im_head.save('clip/cropped_head.jpg')
+    # im_head.save('clip/cropped_head.jpg')
     plot_inference_qa(im_hand, "what color are the fingers?")
+    global objectListMDETR
+    objectListMDETR= im_head
+    objectListMDETR2= []
 
 def loadClip():
     import clip
@@ -566,8 +712,11 @@ def clipImageTk():
     imagenn = ImageTk.PhotoImage(path1)
     iyyey = ImageTk.PhotoImage(path1)
 
+    global printImage
+    printImage = ImageTk.PhotoImage(objectListMDETR)
+
     imagenList=[imagenn]
-    labelimage = Label(clipImageTkinter, image=imagenList)
+    labelimage = Label(clipImageTkinter, image=printImage)
     labelimage.pack()
 
     # buttonBack = Button(clipImageTkinter, text='<<', command=lambda:back).pack()
@@ -723,6 +872,7 @@ imagenClipButton = Button(root, text='Imagen Clip', command=clipImageTk).pack()
 showImageClipButton = Button(root, text='Imagen Clip', command=showImageClipTk).pack()
 loadClipButton = Button(root, text='Cargar Clip', command=loadClip).pack()
 clipButton = Button(root, text='Clip', command=clip).pack()
+MDETRButton = Button(root, text='MDETR', command=MDETR).pack()
 
 
 exitButton = Button(root, text="Salir", command=root.quit)
