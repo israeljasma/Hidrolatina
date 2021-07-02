63bbbbcaf8e62edfe07d89a9643ccb78339137f7
Israel Jasma <by.darklancer@gmail.com>
2021-07-02 01:30:02 -0400

    Feat: class imagenClipClass

---
+++ imagenClipClass.py
@@ -0,0 +1,10 @@
+class imageClip:
+    def __init__(self, image, answer):
+        self.image = image
+        self.aswer = answer
+
+    def getImage(self):
+        return self.image
+
+    def getAnswer(self):
+        return self.aswer
