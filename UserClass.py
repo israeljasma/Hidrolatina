4bcff229513cad6b251698ea730391cd784dc203
Israel Jasma <israel.jasma@gmail.com>
2021-11-04 13:48:34 -0300

    Fix: name corregido

--- UserClass.py
+++ UserClass.py
@@ -1,13 +1,17 @@
 class Person:
-    def __init__(self, username, last_name, email, token):
+    def __init__(self, username, name, last_name, email, token):
         
         self.username = username
+        self.name = name
         self.last_name = last_name
         self.email = email
         self.token = token
 
     def getUsername(self):
         return self.username
+
+    def getName(self):
+        return self.name
 
     def getLast_name(self):
         return self.last_name
