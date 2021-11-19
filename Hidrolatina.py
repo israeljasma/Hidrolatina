8b9a31ebfee000d7730321f9b8a0389f2485b833
Israel Jasma <by.darklancer@gmail.com>
2021-11-19 18:14:40 -0300

    Feat: Depurando codigo

--- Hidrolatina.py
+++ Hidrolatina.py
@@ -461,9 +461,9 @@ def showPytorchCameraTk(user):
                 copy_imgtk = imgtk
                 labelVideo.imgtk = copy_imgtk
                 mdetr_list=MDETR(im)
-################################################ CORRERGIR ###############################################
-################################################ CORRERGIR ###############################################
-################################################ CORRERGIR ###############################################
+    ################################################ CORRERGIR ###############################################
+    ################################################ CORRERGIR ###############################################
+    ################################################ CORRERGIR ###############################################
                 # print(mdetr_list)
                 global listImagenClip
                 listImagenClip = []
@@ -472,9 +472,9 @@ def showPytorchCameraTk(user):
                     # print(bodypart)
                 
                 updateLabel()
-################################################ CORRERGIR ###############################################
-################################################ CORRERGIR ###############################################
-################################################ CORRERGIR ###############################################
+    ################################################ CORRERGIR ###############################################
+    ################################################ CORRERGIR ###############################################
+    ################################################ CORRERGIR ###############################################
                 
         if det<20:
             labelVideo.after(10, showFrame)
@@ -579,49 +579,6 @@ def configCameraTk(configurationTk):
     #Buttons Tk
     closeWindow = Button(configCameraTk, text="Cerrar Ventana", command=lambda:closeTk(configurationTk))
     closeWindow.pack()
-
-def identify():
-    from smartcard.CardRequest import CardRequest
-    from smartcard.Exceptions import CardRequestTimeoutException
-    from smartcard.CardType import AnyCardType
-    from smartcard import util
-    import time
-    WAIT_FOR_SECONDS = 60
-    # respond to the insertion of any type of smart card
-    card_type = AnyCardType()
-
-    # create the request. Wait for up to x seconds for a card to be attached
-    request = CardRequest(timeout=WAIT_FOR_SECONDS, cardType=card_type)
-
-    while True:
-        # listen for the card
-        service = None
-        try:
-            service = request.waitforcard()
-        except CardRequestTimeoutException:
-            print("Tarjeta no detectada")
-            # could add "exit(-1)" to make code terminate
-
-        # when a card is attached, open a connection
-        try:
-            conn = service.connection
-            conn.connect()
-
-            # get the ATR and UID of the card
-            get_uid = util.toBytes("FF CA 00 00 00")
-            data, sw1, sw2 = conn.transmit(get_uid)
-            uid = util.toHexString(data)
-            status = util.toHexString([sw1, sw2])
-            if uid == "44 CE 4A 0B":
-
-            # print the ATR and UID of the card
-            # print("ATR = {}".format(util.toHexString(conn.getATR())))
-                print("Operador Reconocido")
-                break
-        except:
-            pass
-    time.sleep(2)
-    # showPytorchCameraTk()
 
 def nfc_identifyTk():
     # import concurrent.futures
---
+++ PpeDetector.py
@@ -0,0 +1,171 @@
+import re
+import requests
+import pathlib
+from collections import defaultdict
+
+import torch
+import torchvision.transforms as T
+import clip as clipit
+
+from effdet.utils.inference import init_effdet_model,inference_effdet_model
+
+class PpeDetector:
+
+    def __init__(self):
+        self.importMDETR = self.importMDETR()
+    
+    def argmax(iterable):
+            return max(enumerate(iterable), key=lambda x: x[1])[0]
+
+    class importMDETR:
+
+        def __init__(self):
+
+            torch.set_grad_enabled(False);
+            temp = pathlib.PosixPath
+            pathlib.PosixPath = pathlib.WindowsPath
+
+            model, postprocessor = torch.hub.load('ashkamath/mdetr:main', 'mdetr_efficientnetB5', pretrained=True, return_postprocessor=True)
+            self.model = model.cuda()
+            self.model.eval();
+
+            # global transform, box_cxcywh_to_xyxy, rescale_bboxes, COLORS, plot_results, id2answerbytype, plot_inference, plot_inference_qa
+            # standard PyTorch mean-std input image normalization
+            self.transform = T.Compose([
+                T.Resize(800),
+                T.ToTensor(),
+                T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ])
+
+        # for output bounding box post-processing
+        def box_cxcywh_to_xyxy(self, x):
+            x_c, y_c, w, h = x.unbind(1)
+            b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+                (x_c + 0.5 * w), (y_c + 0.5 * h)]
+            return torch.stack(b, dim=1)
+
+        def rescale_bboxes(self, out_bbox, size):
+            img_w, img_h = size
+            b = self.box_cxcywh_to_xyxy(out_bbox)
+            b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+            return b
+
+        # colors for visualization
+        COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+                [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+
+        import json
+        answer2id_by_type = json.load(requests.get("https://nyu.box.com/shared/static/j4rnpo8ixn6v0iznno2pim6ffj3jyaj8.json", stream=True).raw)
+        id2answerbytype = {}                                                       
+        for ans_type in answer2id_by_type.keys():                        
+            curr_reversed_dict = {v: k for k, v in answer2id_by_type[ans_type].items()}
+            id2answerbytype[ans_type] = curr_reversed_dict 
+
+
+        def plot_inference(self, im, caption):
+        # mean-std normalize the input image (batch-size: 1)
+            img = self.transform(im).unsqueeze(0).cuda()
+
+            # propagate through the model
+            memory_cache = self.model(img, [caption], encode_and_save=True)
+            outputs = self.model(img, [caption], encode_and_save=False, memory_cache=memory_cache)
+
+            # global probas, keep
+            # keep only predictions with 0.7+ confidence
+            self.probas = 1 - outputs['pred_logits'].softmax(-1)[0, :, -1].cpu()
+            self.keep = (self.probas > 0.7).cpu()
+
+            # convert boxes from [0; 1] to image scales
+            bboxes_scaled = self.rescale_bboxes(outputs['pred_boxes'].cpu()[0, self.keep], im.size)
+
+            # Extract the text spans predicted by each box
+            positive_tokens = (outputs["pred_logits"].cpu()[0, self.keep].softmax(-1) > 0.1).nonzero().tolist()
+            predicted_spans = defaultdict(str)
+            for tok in positive_tokens:
+                item, pos = tok
+                if pos < 255:
+                    span = memory_cache["tokenized"].token_to_chars(0, pos)
+                    predicted_spans [item] += " " + caption[span.start:span.end]
+            labels = [predicted_spans [k] for k in sorted(list(predicted_spans .keys()))]
+            # global bboxes
+            self.bboxes=bboxes_scaled.numpy()
+            # print('boxes: ', bboxes)
+            # plot_results(im, probas[keep], bboxes_scaled, labels)
+
+            return (self.bboxes[PpeDetector.argmax(self.probas[self.keep])])
+
+        print("MDETR cargado")
+    
+
+    def clearCacheMDETR(self):
+        torch.cuda.empty_cache()
+
+    def loadEfficient(self):
+        self.weigths_effdet='https://github.com/EquipoVandV/EfficientDetVandV/blob/main/effdet/logs/person_coco/efficientdet-d2_58_8260_best.pth'
+        # global obj_list
+        self.obj_list = ['person']
+        # global model_effdet
+        self.model_effdet = init_effdet_model(self.weigths_effdet, self.obj_list)
+
+        print('EfficientDET Cargado')
+
+    def MDETR(self, im):
+        bboxes_body = self.importMDETR.plot_inference(im, "a hand")
+        # plot_inference(im, "a hand")
+        im_hand=im.crop(bboxes_body)
+
+        bboxes_body = self.importMDETR.plot_inference(im, "a head")
+        # plot_inference(im, "a head")
+        im_head=im.im.crop(bboxes_body)
+        
+        bboxes_body = self.importMDETR.plot_inference(im, "a boot")
+        im_boot=im.crop(bboxes_body)
+
+        objectListMDETR= {'im_head':im_head, 'im_hand': im_hand, 'im_boot': im_boot}
+        return objectListMDETR
+
+    def loadClip(self):
+        # global clipit
+
+        # global candidate_captions
+        self.candidate_captions={'im_head': [['a head with a yellow helmet','Just a head'], ['Head with headphones', 'Just a head'],['a Head with goggles', 'Just a head'],['Head with a medical mask', 'Just a head']],
+                    'im_hand':[['A blue hand', 'A pink hand']],
+                    'im_boot':[['A black boot', 'A shoe']]}
+        # candidate_captions={'im_head': [['a white hat','A head'], ['a big headset', 'a face'],['a face with glasses', 'A head'],['Mask', 'Just a head']],
+        #             'im_hand':[['A blue hand', 'A pink hand']],
+        #             'im_boot':[['a large boot', 'a small shoe']]}
+        # global names_ppe
+        self.names_ppe = {'im_head': ['Casco', 'Audífonos', 'Antiparras', 'Mascarilla'], 'im_hand': ['Guantes'], 'im_boot': ['Botas']}
+
+        # global device
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        # device = "cpu"
+
+        # global modelc, process
+        self.modelc, self.process = clipit.load("ViT-B/32", device=self.device)
+
+        # global nstr
+        # def nstr(obj):
+        #     return [name for name in globals() if globals()[name] is obj][0]
+        print('Clip Cargado')
+
+    def clip(self, bodypart, mdetr_list):
+        pred_clip=[]
+        for i in range(len(self.candidate_captions[bodypart])):
+            text = clipit.tokenize(self.candidate_captions[bodypart][i]).to(self.device)
+            image = self.process(mdetr_list[bodypart]).unsqueeze(0).to(self.device)
+
+            with torch.no_grad():
+                # image_features = modelc.encode_image(image)
+                # text_features = modelc.encode_text(text)
+                
+                logits_per_image, logits_per_text = self.modelc(image, text)
+                probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+                # pred = class_names[argmax(list(probs)[0])]
+                if self.argmax(list(probs)[0])== 0:
+                    pred_clip.append('OK')
+                else:
+                    pred_clip.append('NO DETECTADO')
+
+        return pred_clip
