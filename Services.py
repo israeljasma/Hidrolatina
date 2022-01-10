d5e104a8c35128dc296fbc697fa292b7dfc2590b
Israel Jasma <by.darklancer@gmail.com>
2022-01-10 01:49:21 -0300

    Feat: servicio logout

--- Services.py
+++ Services.py
@@ -15,6 +15,13 @@ class API_Services():
         request_dictionary = request.json()
         return request_dictionary
 
+    def logout(token, refresh):
+        credentials = {'refresh': refresh}
+        request = requests.post(url + 'logout/',data = credentials, headers={'Authorization': 'Bearer '+token})
+        request_dictionary = request.json()
+        print(request_dictionary)
+        return request_dictionary
+
     def ppeDetection(helmet, headphones, goggles, gloves, boots, token):
         credentials = {'helmet':helmet, 'headphones':headphones, 'goggles':goggles, 'gloves':gloves, 'boots':boots, 'token':token}
         request = requests.post('http://127.0.0.1:8000/ppe/ppe/',data = credentials)
